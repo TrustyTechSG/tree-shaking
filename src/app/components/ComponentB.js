c9ae6dff6f92b61bb48a49ba95cb94b3d51fbb47
TrustyTech <trustytech.master@gmail.com>
2023-05-13 16:14:46 +0800

    make component bigger

--- src/app/components/ComponentB.js
+++ src/app/components/ComponentB.js
@@ -1,28 +1,90 @@
 "use client";
 
 const stats = [
-  { name: 'Number of deploys', value: '405' },
-  { name: 'Average deploy time', value: '3.65', unit: 'mins' },
-  { name: 'Number of servers', value: '3' },
-  { name: 'Success rate', value: '98.5%' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
+  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
+  { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
+  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
 ]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export function ComponentB() {
   return (
-    <div className="bg-gray-900">
-      <div className="mx-auto max-w-7xl">
-        <div className="grid grid-cols-1 gap-px bg-white/5 sm:grid-cols-2 lg:grid-cols-4">
-          {stats.map((stat) => (
-            <div key={stat.name} className="bg-gray-900 px-4 py-6 sm:px-6 lg:px-8">
-              <p className="text-sm font-medium leading-6 text-gray-400">{stat.name}</p>
-              <p className="mt-2 flex items-baseline gap-x-2">
-                <span className="text-4xl font-semibold tracking-tight text-white">{stat.value}</span>
-                {stat.unit ? <span className="text-sm text-gray-400">{stat.unit}</span> : null}
-              </p>
-            </div>
-          ))}
+    <dl className="mx-auto grid grid-cols-1 gap-px bg-gray-900/5 sm:grid-cols-2 lg:grid-cols-4">
+      {stats.map((stat) => (
+        <div
+          key={stat.name}
+          className="flex flex-wrap items-baseline justify-between gap-x-4 gap-y-2 bg-white px-4 py-10 sm:px-6 xl:px-8"
+        >
+          <dt className="text-sm font-medium leading-6 text-gray-500">{stat.name}</dt>
+          <dd
+            className={classNames(
+              stat.changeType === 'negative' ? 'text-rose-600' : 'text-gray-700',
+              'text-xs font-medium'
+            )}
+          >
+            {stat.change}
+          </dd>
+          <dd className="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">
+            {stat.value}
+          </dd>
         </div>
-      </div>
-    </div>
+      ))}
+    </dl>
   )
 }
